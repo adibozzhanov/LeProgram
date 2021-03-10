2cfc6d4a84b2a97226ed113992174d26ddb94785
adi-bozzhanov <adi.bozzhanov.20@ucl.ac.uk>
2021-03-10 00:47:38 +0000

    feat: some unfinished stuff

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,7 +1,52 @@
 import sys
 from views import view
 
-class Controller:    
+class Controller:
+    def __init_(self):
+        pass
+
+    
     def run(self):
         myView = view.View()
         myView.run()
+
+
+
+
+    def nameIsValid(self, name):
+        # name: string
+        # returns: boolean
+
+        pass
+
+    
+    def validateExpression(self, expr):
+        # expr: string of the expression
+        # returns:   incorrect -> False
+        #              correct -> [AST]
+
+        pass
+
+    def createLibrary(self, fields):
+        # checks if the name of the library is valid and then
+        
+        pass
+
+    def createTask(self, fields):
+        # 
+        pass
+
+
+    # controller - Model communication
+
+    def getLibrary(self):
+
+        pass
+
+
+    def getTask(self,id):
+
+        pass
+
+    def get
+        
--- src/views/view.py
+++ src/views/view.py
@@ -3,18 +3,38 @@ from views.main import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+# Frames:
+# Main Frame - gets constanty reloaded by user's actions
+# Top Frame - always there on top
+
+
+
+
+
 class View:
     def __init__(self):
+
+        # key  
+        self.displays = {} 
         self.app = QtWidgets.QApplication(sys.argv)
         self.MainWindow = QtWidgets.QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.MainWindow)
-        self.ui.pushButton.clicked.connect(self.changeLabel)
+
+    
+
 
     def run(self):
         self.MainWindow.show()
         sys.exit(self.app.exec_())
 
 
-    def changeLabel(self):
-        self.ui.label.setText("Hey hot stuff;)")
+    def updateMain(self, displayName):
+        # clears the main frame and loads new widgets in it
+        pass
+
+
+    def cleanMain(self):
+        # removes all elements from the main frame
+        
+        pass
