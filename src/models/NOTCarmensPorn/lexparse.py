d76d1ea68cd017c6770ff556407bcfc0e65e4c54
cmeinson <carmen.meinson.20@ucl.ac.uk>
2021-03-11 01:42:49 +0000

    feat: truth table and bit ugly DFA

--- src/models/NOTCarmensPorn/lexparse.py
+++ src/models/NOTCarmensPorn/lexparse.py
@@ -13,29 +13,66 @@ class Node(object): #can later add the function right to the node
     def __init__(self, data, symbol, formula):
         self.data = data
         self.children = []
+        self.variables = set()
         self.formula = formula #input is dict and output bool
         self.symbol = symbol #string:)
+        self.str = None
 
-    def add_child(self, obj):
+    def addChild(self, obj):
         self.children.append(obj)
 
-    def evaluate(self, dict):
+    def getVariables(self):
+        return self.variables.copy()
+
+    def getExpressionString(self):
+        if not self.str: self.str = self.createString()
+        return self.str
+
+    def getTableRow(self, dict): #returns all values (I guess this type of stuff could be useful for logic gates)
+        # [total value, [all values for the table], index of last value]
+        if len(self.children)==2:
+            left = self.children[0].getTableRow(dict)
+            right = self.children[1].getTableRow(dict)
+            current = self.formula(left[0],right[0])
+            return [current, [None] + left[1] + [None,current,None] + right[1] + [None], len(left[1])+2]
+        elif len(self.children)==1:
+            right = self.children[0].getTableRow(dict)
+            current = self.formula(right[0])
+            return [current, [current] + right[1], 0]
+        elif len(self.children)==0:
+            return [self.formula(self.data, dict),[None],0]
+        else:
+            print("ERROR:) too many kids hun")
+
+
+    def evaluate(self, dict): #this only returns the final value ATM USELESS
         if len(self.children)==2:
             return self.formula(self.children[0].evaluate(dict),self.children[1].evaluate(dict))
         elif len(self.children)==1:
             return self.formula(self.children[0].evaluate(dict))
         elif len(self.children)==0:
             return self.formula(self.data, dict)
-
         else:
             print("ERROR:) too many kids hun")
 
-    #def evaluate(self, dict):
-        #return self.
+    def createString(self):
+        if len(self.children)==2:
+            return ("(" + self.children[0].createString() + " " +self.symbol + " " + self.children[1].createString() + ")")
+        elif len(self.children)==1:
+            return ( self.symbol + self.children[0].createString() )
+        elif len(self.children)==0:
+            return (self.symbol)
+        else:
+            print("ERROR:) too many kids hun")
+
+    def updateVariables(self):
+        for child in self.children:
+            self.variables.update(child.variables)
 
 
 #/NOT (/NOT a /OR a /AND /BOT) /IMP a /OR /NOT /NOT /TOP
-# → ↔ ¬ ∧ ∨ ⊤ ⊥
+# → ↔ ¬ ∧ ∨ ⊤ ⊥ (¬((¬a∧a)∧⊥)→(a⊕¬¬⊤)) (¬((¬a ∧ a) ∧ ⊥) → (a ⊕ ¬¬⊤))
+
 #/NOT (/NOT a /AND a /AND /BOT) /IMP a /XOR /NOT /NOT /TOP
 
 
@@ -58,7 +95,7 @@ class BasicLexer(Lexer):
 
 
 class BasicParser(Parser):
-    debugfile = 'parser.out'
+    #debugfile = 'parser.out'
     tokens = BasicLexer.tokens
 
     precedence = (
@@ -71,7 +108,9 @@ class BasicParser(Parser):
 
     @_('NAME')
     def ex(self, p):
-        return Node(p.NAME, p.NAME, lambda name, dict: dict[name])
+        n = Node(p.NAME, p.NAME, lambda name, dict: dict[name])
+        n.variables.add(p.NAME)
+        return n
 
     @_('TOP')
     def ex(self, p):
@@ -88,48 +127,62 @@ class BasicParser(Parser):
     @_('NOT ex')
     def ex(self, p):
         n = Node(p.NOT, "¬", lambda b: not b)
-        n.add_child(p.ex)
+        n.addChild(p.ex)
+        n.updateVariables()
         return n
 
     @_('ex AND ex')
     def ex(self, p):
         n = Node(p.AND, "∧", lambda b1, b2: b1 and b2)
-        n.add_child(p.ex0)
-        n.add_child(p.ex1)
+        n.addChild(p.ex0)
+        n.addChild(p.ex1)
+        n.updateVariables()
         return n
 
     @_('ex OR ex')
     def ex(self, p):
         n = Node(p.OR, "∨", lambda b1, b2: b1 or b2)
-        n.add_child(p.ex0)
-        n.add_child(p.ex1)
+        n.addChild(p.ex0)
+        n.addChild(p.ex1)
+        n.updateVariables()
         return n
 
     @_('ex XOR ex')
     def ex(self, p):
         n = Node(p.XOR, "⊕", lambda b1, b2: b1 != b2)
-        n.add_child(p.ex0)
-        n.add_child(p.ex1)
+        n.addChild(p.ex0)
+        n.addChild(p.ex1)
+        n.updateVariables()
         return n
 
     @_('ex IFF ex')
     def ex(self, p):
         n = Node(p.IFF, "↔", lambda b1, b2: b1 == b2)
-        n.add_child(p.ex0)
-        n.add_child(p.ex1)
+        n.addChild(p.ex0)
+        n.addChild(p.ex1)
+        n.updateVariables()
         return n
 
     @_('ex IMP ex')
     def ex(self, p):
         n = Node(p.IMP, "→", lambda b1, b2: (not b1) or b2)
-        n.add_child(p.ex0)
-        n.add_child(p.ex1)
+        n.addChild(p.ex0)
+        n.addChild(p.ex1)
+        n.updateVariables()
         return n
 
+def getExpressionTree(inputString):
+    lexer = BasicLexer()
+    parser = BasicParser()
+    try:
+        tree = parser.parse(lexer.tokenize(inputString))
+        pprint_tree(tree)
+    except:
+        print("Parsing failed, btw how we doing errors:)")
+        tree = None
+    return(tree)
 
-
-
-
+'''
 if __name__ == '__main__':
     lexer = BasicLexer()
     parser = BasicParser()
@@ -139,7 +192,11 @@ if __name__ == '__main__':
         except EOFError:
             break
         if text:
-            tree = parser.parse(lexer.tokenize(text))
-            pprint_tree(tree)
+            try:
+                tree = parser.parse(lexer.tokenize(text))
+                pprint_tree(tree)
+                print(tree)
+            except: print("whooopsie")'''
 
-            print(tree.evaluate({'a':True}))
+            #print(tree.getTableRow({'a':True}))
+            #print(tree.getExpressionString())
