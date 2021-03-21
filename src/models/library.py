ed7a40c7e3c443e765ff21d4d5df8a20e7b9c83c
adi-bozzhanov <adi.bozzhanov.20@ucl.ac.uk>
2021-03-21 21:34:57 +0000

    fix: reverted oscars library that I accidentally deleted

--- src/models/library.py
+++ src/models/library.py
@@ -1,32 +1,40 @@
-from models.test import Test
-from models.database import Database
+from test import Test
+from database import Database
 
 class Library:
     def __init__(self, libraryId=None):
+
+        self.lepDB = Database()
+
         if libraryId is None:
             self.name = None
             self.tests = {}
             self.user = None
             # ADD A NEW LIB IN THE DATABASE
-            self.id = 0 # GET THE NEW ID FROM THE DATABASE
+            self.id = self.lepDB.addNewLibraryDB() # GET THE NEW ID FROM THE DATABASE
         else:
             self.id = libraryId
             self.tests = None
             # LOAD DATA FROM THE DATABASE
+            libraryInfoTuple = self.lepDB.loadLibraryDB(self.id)
+            self.name = libraryInfoTuple[1]
 
     def setName(self, newName):
         self.name = newName
         # UPDATE THE DATABASE
+        self.lepDB.updateLibraryNameDB(self.id, newName)
 
     def addTestAndGetId(self):
         t = Test()
         self.tests[t.getTestId()] = t
-        # UPDATE THE DATABASE
+        # UPDATE THE DATABASE update the joining table
+        self.lepDBAddTestIDandLibraryIDtoTesttoLibraryTableDB(t.getTestId(), self.id)
         return t.getTestId()
 
     def removeTest(self, testId):
         del self.tests[testId]
         # UPDATE THE DATABASE
+        self.deleteTestToLibraryDB(testId)
 
     def getLibraryId(self):
         return self.id
@@ -36,16 +44,16 @@ class Library:
 
     def getTest(self, testId):
         if self.tests is None:
-            ids=[] #GET ALL THE ANSWER IDS FROME THE DATABASE
+            ids=[testId] #GET ALL THE TEST IDS FROME THE DATABASE
             for i in ids:
-                self.__loadTest(i)
+                self.__loadTest(i[0])
         return self.tests[testId]
 
     def getTests(self):
         if self.tests is None:
-            ids=[] #GET ALL THE ANSWER IDS FROME THE DATABASE
+            ids=self.lepDB.getTestIDFromLibraryIDDB(self.id) #GET ALL THE TEST IDS FROME THE DATABASE
             for i in ids:
-                self.__loadTest(i)
+                self.__loadTest(i[0])
         return self.tests
 
     def __loadTest(self, testId):
