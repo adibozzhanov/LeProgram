29dee1f685e1f2341925daee79fe1704e83cef55
OscarL63 <zcabole@ucl.ac.uk>
2021-03-23 17:49:52 +0000

    feat: Added the function to change the labels in askLepPage

--- src/views/view.py
+++ src/views/view.py
@@ -38,13 +38,6 @@ class View(Ui_MainWindow):
         # assigns request handler controller method to "self.request"
         self.request = handler
 
-<<<<<<< HEAD
-=======
-    def loadRandomTest(self, taskGenerator):
-        pass
->>>>>>> 718e3c5baed687603a0dce6877d8e1dd8bf2e0c1
-
-
     def loadHome(self, library):
         self.cleanMain()
         self.currentDisplay = Home(self.masterFrame, self, library)
