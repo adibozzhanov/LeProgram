6bfd0895b8b2665fa3c4a46c291594a8e7eab9f6
Clarissa Sandejas <clarissasandejas@gmail.com>
2021-03-24 00:33:02 +0800

    adding tasks works

--- src/views/py/testMaking.py
+++ src/views/py/testMaking.py
@@ -8,6 +8,7 @@ from views.py.askLepWidget import AskLepWidget
 class TestMakingPage(Ui_testMakingPage):
     def __init__(self, master, view):
         self.view = view
+        self.tasks = []
         self.setupUi(master)
         self.connectActions()
         self.addAskLepWidget()
@@ -17,10 +18,10 @@ class TestMakingPage(Ui_testMakingPage):
         self.addTaskButton.clicked.connect(lambda: self.addTaskMakingFrame()) # replace with controller things!
        # self.askLepButton.clicked.connect()
 
-
     def addAskLepWidget(self, expression=None):
         AskLepWidget(self.askLepWidgetFrame, self.view, None)
 
     def addTaskMakingFrame(self):
         newFrame = QtWidgets.QFrame()
-        TaskMakingFrame(self.taskMakingScrollArea, self.view)
+        self.taskScrollBarContents.addWidget(newFrame)
+        self.tasks.append(TaskMakingFrame(newFrame, self.view))
