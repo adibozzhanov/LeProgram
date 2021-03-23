55042ab0f27cd91803b568c549afc7a96225ceee
adi-bozzhanov <adi.bozzhanov.20@ucl.ac.uk>
2021-03-23 16:32:26 +0000

    feat: random test start button sends request

--- src/views/py/randomPage.py
+++ src/views/py/randomPage.py
@@ -6,25 +6,22 @@ class RandomPage(Ui_randomQuestionFrame):
     def __init__(self, master, view):
         self.view = view
         self.setupUi(master)
+        self.complexity = 1
         self.startButton.clicked.connect(lambda: self.view.request("startRandomTest", self.getComplexity()))
-        self.radioButtons = []
-        self.radioButtons.append(self.radioButton)
-        self.radioButtons.append(self.radioButton_2)
-        self.radioButtons.append(self.radioButton_3)
-        self.radioButtons.append(self.radioButton_4)
-        self.radioButtons.append(self.radioButton_5)
-        self.radioButtons.append(self.radioButton_6)
+        self.radioButton.toggled.connect(lambda: self.toggledComplexity(self.radioButton))
+        self.radioButton_2.toggled.connect(lambda: self.toggledComplexity(self.radioButton_2))
+        self.radioButton_3.toggled.connect(lambda: self.toggledComplexity(self.radioButton_3))
+        self.radioButton_4.toggled.connect(lambda: self.toggledComplexity(self.radioButton_4))
+        self.radioButton_5.toggled.connect(lambda: self.toggledComplexity(self.radioButton_5))
+        self.radioButton_6.toggled.connect(lambda: self.toggledComplexity(self.radioButton_6))
+
+    def toggledComplexity(self, radio):
+        if radio.isChecked() == True:
+            self.complexity = int(radio.text())
         
-        for radio in self.radioButtons:
-            radio.toggled.connect(lambda: self.toggledComplexity())
-
-
-    def toggledComplexity(self):
-
-        pass
 
     def getComplexity(self):
-        pass 
+        return self.complexity
 
         
 
