6e8af9778e11b53697f95b42eaf68ec281a7eb66
OscarL63 <zcabole@ucl.ac.uk>
2021-03-24 15:38:43 +0000

    fix: fixed the icons by deleting the ui\\ in all of the icon file paths

--- src/views/py/taskMaking.py
+++ src/views/py/taskMaking.py
@@ -3,6 +3,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from views.py.taskMakingQt import Ui_taskMakingFrame
 
 class TaskMakingFrame(Ui_taskMakingFrame):
-    def __init__(self, master, view):
+    def __init__(self, master, view, test, taskId):
         self.view = view
+        self.test = test
+        self.taskId = taskId
         self.setupUi(master)
+        self.connectActions()
+
+    def connectActions(self):
+        self.deleteButton.clicked.connect(lambda: self.test.remove(taskId))
