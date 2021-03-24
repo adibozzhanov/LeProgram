38df0d272ca931f07cc09a889a77c0f58227ad5e
Clarissa Sandejas <clarissasandejas@gmail.com>
2021-03-25 00:01:21 +0800

    BAP

--- src/views/py/testMaking.py
+++ src/views/py/testMaking.py
@@ -13,7 +13,6 @@ class TestMakingPage(Ui_testMakingPage):
         self.view = view
         self.testID = library.addNewTestAndGetId()
         self.test = library.getTest(self.testID)
-        self.test.setName("CLARISSA'S DOPE TEST")
 
         self.taskIDs = []
         self.setupUi(master)
@@ -23,6 +22,7 @@ class TestMakingPage(Ui_testMakingPage):
 
     def connectActions(self):
         self.addTaskButton.clicked.connect(lambda: self.addTaskMakingFrame())
+        self.testNameInput.textChanged.connect(lambda: self.renameTest())
        # self.askLepButton.clicked.connect()
 
     def addAskLepWidget(self, expression=None):
@@ -33,3 +33,10 @@ class TestMakingPage(Ui_testMakingPage):
         self.taskScrollBarContents.addWidget(newFrame)
         self.taskIDs.append(self.test.addNewTaskAndGetId())
         TaskMakingFrame(newFrame, self.view, self.test, self.taskIDs[-1])
+
+    def renameTest(self):
+        text = self.testNameInput.toPlainText()
+        text.replace("\n","")
+        text.replace("\r", "")
+        self.test.setName(text)
+        self.testNameLabel.setText(text)
