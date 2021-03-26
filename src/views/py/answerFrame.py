468a1fa5f25f46543b6533fe7d2720b911b62ded
adi-bozzhanov <adi.bozzhanov.20@ucl.ac.uk>
2021-03-26 17:04:45 +0000

    feat: progress bar and colorful buttons

--- src/views/py/answerFrame.py
+++ src/views/py/answerFrame.py
@@ -4,9 +4,21 @@ from views.py.answerFrameQt import Ui_answerFrame
 
 
 class AnswerFrame(Ui_answerFrame):
-    def __init__(self, master, view, answer):
+    def __init__(self, master, view, answer, taskFrame):
+        self.taskFrame = taskFrame
         self.view = view
         self.setupUi(master)
+        self.answer = answer
         self.answerButton.setText(answer.getExpression().getDisplayString())
+        self.answerButton.clicked.connect(self.onClick)
+
+    def onClick(self):
+        if self.answer.getIsCorrect():
+            self.answerButton.setStyleSheet("background-color : green")
+        else:
+            self.answerButton.setStyleSheet("background-color : red")
+        
+        self.taskFrame.checkAnswers(self.answer.getAnswerId())
+        
         
     
--- src/views/py/taskFrame.py
+++ src/views/py/taskFrame.py
@@ -7,10 +7,13 @@ from views.py.answerFrame import AnswerFrame
 
 class TaskFrame(Ui_taskFrame):
 
-    def __init__(self, master, view, task):
+    def __init__(self, master, view, task, testFrame):
         self.view = view
         self.setupUi(master)
+        self.testFrame = testFrame
+        self.task = task
         answers = task.getAnswers()
+        self.answerFrames = []
         self.taskStatementLabel.setText(task.getStatement())
         #self.expressionLabel.setText()
         for Id,answer in answers.items():
@@ -19,4 +22,12 @@ class TaskFrame(Ui_taskFrame):
     def addAnswer(self, answer):
         newFrame = QtWidgets.QFrame()
         self.answersLayout.addWidget(newFrame)
-        AnswerFrame(newFrame, self.view, answer)
+        tmp = AnswerFrame(newFrame, self.view, answer, self)
+        self.answerFrames.append(tmp)
+
+    def checkAnswers(self, answerId):
+        for answerFrame in self.answerFrames:
+            if answerFrame.answer.getIsCorrect():
+                answerFrame.answerButton.setStyleSheet("background-color : green")
+        self.testFrame.test.addTaskAnswerGiven(self.task.getTaskId(), answerId)
+        self.testFrame.nextQButton.setEnabled(True)
